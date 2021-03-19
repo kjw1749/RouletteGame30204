407bf8255103316f72fdd82a99ffa2bf4fca6386
kjw1749 <80082529+kjw1749@users.noreply.github.com>
2021-03-19 11:52:22 +0900

    Initial commit

---
+++ Assets/Script/Roulette.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roulette : MonoBehaviour
+{
+    float rotSpeed = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10.0f;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        if (Input.GetMouseButton(1))
+        {
+            this.rotSpeed = 0f;
+
+        }
+        this.rotSpeed *= 0.99f;
+    }
+}
